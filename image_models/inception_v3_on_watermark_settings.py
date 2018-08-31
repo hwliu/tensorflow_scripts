62d4661326535b839930a3f9e8cff2b7cd794e3a
Haowei Liu <haoweiliu@google.com>
2018-08-31 13:46:23 -0700

    change settings

--- image_models/inception_v3_on_watermark_settings.py
+++ image_models/inception_v3_on_watermark_settings.py
@@ -16,6 +16,16 @@ KEYS_TO_FEATURE_MAP = {
             [10], dtype=tf.int64, default_value=[0 for _ in xrange(10)]),
 }
 
-INCEPTION_V3_TARGET_IMAGE_SIZE = numpy.array([299, 299])
+#  To parse Cervalat dataset
+CERVALT_DATASET_IMAGE_KEY = 'image/encoded'
+CERVALT_DATASET_LABEL_KEY = 'image/class/label'
+CERVALT_KEYS_TO_FEATURE_MAP = {
+    CERVALT_DATASET_IMAGE_KEY:
+        tf.FixedLenFeature([], dtype=tf.string, default_value=''),
+    CERVALT_DATASET_LABEL_KEY:
+        tf.FixedLenFeature(
+            [1], dtype=tf.int64, default_value=[0]),
+}
 
+INCEPTION_V3_TARGET_IMAGE_SIZE = numpy.array([299, 299])
 INCEPTION_V3_MODULE_PATH = 'https://tfhub.dev/google/imagenet/inception_v3/feature_vector/1'
