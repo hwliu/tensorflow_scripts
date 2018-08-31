eee329ffacdddbfde457f154b5018a3a2fe2bbd4
Haowei Liu <haoweiliu@google.com>
2018-08-31 13:45:43 -0700

    update functions

--- image_models/input_preprocessing_helpers.py
+++ image_models/input_preprocessing_helpers.py
@@ -1,13 +1,16 @@
 """Utility functions to process the input for inception v3 model."""
 import tensorflow as tf
-#from google3.experimental.users.haoweiliu.watermark_training.inception_v3_on_watermark_settings import DATASET_IMAGE_KEY
-#from google3.experimental.users.haoweiliu.watermark_training.inception_v3_on_watermark_settings import DATASET_LABEL_KEY
 #from google3.experimental.users.haoweiliu.watermark_training.inception_v3_on_watermark_settings import INPUT_FEATURE_NAME
-#from google3.experimental.users.haoweiliu.watermark_training.inception_v3_on_watermark_settings import KEYS_TO_FEATURE_MAP
-from inception_v3_on_watermark_settings import DATASET_IMAGE_KEY
-from inception_v3_on_watermark_settings import DATASET_LABEL_KEY
+#from google3.experimental.users.haoweiliu.watermark_training.example_parser import TFSequenceExampleParser
+#from google3.experimental.users.haoweiliu.watermark_training.example_parser import TFExampleParser
+#from google3.experimental.users.haoweiliu.watermark_training.example_parser import create_dataset_parser
+#from google3.experimental.users.haoweiliu.watermark_training.example_parser import Parser
+
 from inception_v3_on_watermark_settings import INPUT_FEATURE_NAME
-from inception_v3_on_watermark_settings import KEYS_TO_FEATURE_MAP
+from example_parser import TFSequenceExampleParser
+from example_parser import TFExampleParser
+from example_parser import create_dataset_parser
+from example_parser import Parser
 
 
 def shardedfile_to_filelist(filename):
@@ -43,7 +46,7 @@ def preprocess(image, target_image_size):
   # Scale image and convert to float32. image is of type uint8.
   image = tf.image.convert_image_dtype(image, dtype=tf.float32)
   image = crop_and_resize_image(image, target_image_size)
-  image = tf.multiply(tf.subtract(image, 0.5), 2.0)
+  image = tf.subtract(tf.multiply(image, 2.0), 1.0)
   return image
 
 
@@ -55,33 +58,43 @@ def preprocess_image(img_encoded, target_image_size):
   return image
 
 
-def preprocess_label(label):
+def convert_label_indicator(label):
   """Returns 1 if one of the elements is set: 0, 2, 4, 7, 9 in label."""
-  # see https://cs.corp.google.com/piper///depot/google3/experimental/users/bingjian/images/tools/flume_convert_from_sequence_example.cc?ws=haoweiliu/871
+  # See https://cs.corp.google.com/piper///depot/google3/experimental/users/
+  # bingjian/images/tools/flume_convert_from_sequence_example.cc?
+  # ws=haoweiliu/871
   label = tf.gather(tf.cast(label, tf.int32), [0, 2, 4, 7, 9])
   output_label = tf.cast(tf.reduce_any(tf.greater(label, 0)), tf.int32)
   return output_label
 
 
-def filter_empty_feature(value, keys_to_features):
-  context, _ = tf.parse_single_sequence_example(
-      value, context_features=keys_to_features)
+def convert_label_single_value(label):
+  ## note: why 0 instead of [0]?
+  return tf.cast(tf.gather(label, 0), tf.int32)
 
-  raw_bytes = tf.decode_raw(context[DATASET_IMAGE_KEY], tf.uint8)
+
+def convert_label(label):
+  true_fn = (lambda: convert_label_indicator(label))
+  false_fn = (lambda: convert_label_single_value(label))
+  return tf.cond(tf.shape(label)[0] > 1, true_fn=true_fn, false_fn=false_fn)
+
+
+def filter_empty_feature(image, _):
+  raw_bytes = tf.decode_raw(image[INPUT_FEATURE_NAME], tf.uint8)
   return tf.greater(tf.size(raw_bytes), 0)
 
 
-def parser(value, keys_to_features):
+def parser(value, dataset_parser):
   """Parses the raw data from SSTable into image and labels."""
-  context, _ = tf.parse_single_sequence_example(
-      value, context_features=keys_to_features)
-
-  image = {INPUT_FEATURE_NAME: context[DATASET_IMAGE_KEY]}
-  label = preprocess_label(context[DATASET_LABEL_KEY])
+  images, labels = dataset_parser.parse_single_example(value)
+  image = {INPUT_FEATURE_NAME: images}
+  label = convert_label(labels)
   return image, label
 
 
 def create_input_fn_for_images_sstable(input_file_name,
+                                       keys_to_features_map,
+                                       dataset_type,
                                        mode,
                                        batch_size=50,
                                        do_shuffle=True,
@@ -91,45 +104,14 @@ def create_input_fn_for_images_sstable(input_file_name,
                                        element_count=-1,
                                        skip_count=0):
   """Creates and returns an input_fn based on input parameters."""
-  keys_to_features = KEYS_TO_FEATURE_MAP
-  def input_fn():
-    """Creates an input_fn based on input parameters."""
-    parse_fn = (lambda x: parser(x, keys_to_features))
-    input_shard_file_list = shardedfile_to_filelist(input_file_name)
-    with tf.name_scope('read_batch_features'):
-      dataset = tf.data.TFRecordDataset(input_shard_file_list)
-      dataset = dataset.repeat(1)
-      tfrecord = dataset.make_one_shot_iterator().get_next()
-      context, _ = tf.parse_single_sequence_example(
-          tfrecord, context_features=keys_to_features)
-      image = {INPUT_FEATURE_NAME: tf.reshape(context[DATASET_IMAGE_KEY], shape=(-1,))}
-      label = preprocess_label(context[DATASET_LABEL_KEY])
-      label = tf.reshape(label, shape=(-1,))
-      return image, label
+  dataset_parser = create_dataset_parser(dataset_type, keys_to_features_map)
 
-  return input_fn
-
-def create_input_fn_for_images_sstable2(input_file_name,
-                                       mode,
-                                       batch_size=50,
-                                       do_shuffle=True,
-                                       do_filter=True,
-                                       num_parallelism=128,
-                                       queue_capacity=256,
-                                       element_count=-1,
-                                       skip_count=0):
-  """Creates and returns an input_fn based on input parameters."""
-  keys_to_features = KEYS_TO_FEATURE_MAP
   def input_fn():
     """Creates an input_fn based on input parameters."""
     is_training = (mode == tf.estimator.ModeKeys.TRAIN)
-    if is_training:
-      num_epochs = None
-    else:
-      num_epochs = 1
+    num_epochs = None if is_training else 1
 
-    parse_fn = (lambda x: parser(x, keys_to_features))
-    filter_fn = (lambda x: filter_empty_feature(x, keys_to_features))
+    parse_fn = (lambda x: parser(x, dataset_parser))
 
     input_shard_file_list = shardedfile_to_filelist(input_file_name)
     with tf.name_scope('read_batch_features'):
@@ -138,17 +120,19 @@ def create_input_fn_for_images_sstable2(input_file_name,
         dataset = dataset.take(element_count)
       if skip_count > 0:
         dataset = dataset.skip(skip_count)
-      if do_filter:
-        dataset = dataset.filter(filter_fn)
-      dataset = dataset.repeat(num_epochs)
       if do_shuffle:
         dataset = dataset.shuffle(buffer_size=queue_capacity)
-      dataset = dataset.map(
-          parse_fn, num_parallel_calls=num_parallelism).batch(batch_size)
+
+      dataset = dataset.map(parse_fn, num_parallel_calls=num_parallelism)
+      if do_filter:
+        dataset = dataset.filter(filter_empty_feature)
+
+      dataset = dataset.repeat(num_epochs).batch(batch_size)
       images, labels = dataset.make_one_shot_iterator().get_next()
       return images, labels
 
   return input_fn
+
 
 def exported_model_input_signature():
   inputs = {INPUT_FEATURE_NAME: tf.placeholder(tf.string, shape=(None,))}
